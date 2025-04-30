5974cf7c3e017a835eada3b139d227848f7f7073
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-04-30 03:47:07 -0400

    Added models on gisapp for expanding User through UserProfile

--- gisproject/gisapp/models.py
+++ gisproject/gisapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    home_address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    location = gis_models.PointField(geography=True)
+
+    def __str__(self):
+        return self.user.username
