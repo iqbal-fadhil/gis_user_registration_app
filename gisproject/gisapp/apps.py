ebf8fa163af826e1752fedf83f19fe82bcf8d7f4
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-04-30 03:53:50 -0400

    Added signals on gisapp for profile creation

--- gisproject/gisapp/apps.py
+++ gisproject/gisapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GisappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gisapp'
+
+    def ready(self):
+        import gisapp.signals
