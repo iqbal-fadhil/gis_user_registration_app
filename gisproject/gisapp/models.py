de605c1782b68b747dafd841cacd299096c110a5
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-05-01 00:08:11 -0400

    Changed location attributes to null and blank True

--- gisproject/gisapp/models.py
+++ gisproject/gisapp/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     home_address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    location = gis_models.PointField(geography=True)
+    location = gis_models.PointField(geography=True, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
--- gisproject/gisproject/settings.py
+++ gisproject/gisproject/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'gisproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gisapp_db2',
+        'USER': 'gis_user',
+        'PASSWORD': 'GISgis2025!!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
