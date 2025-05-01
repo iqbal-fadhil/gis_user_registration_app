a4d1a0f196105a0a18f8f3537b92d35a3e388e8b
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-05-01 12:12:16 -0400

    Fixed Edit Profile not showing pin point

--- gisproject/gisapp/views.py
+++ gisproject/gisapp/views.py
@@ -7,14 +7,23 @@ from .forms import UserForm, UserProfileForm
 def profile_view(request):
     return render(request, 'profile.html', {'user': request.user})
 
+from django.contrib.gis.geos import GEOSGeometry
+
 @login_required
 def edit_profile(request):
     user_form = UserForm(instance=request.user)
-    profile_form = UserProfileForm(instance=request.user.userprofile)
+    profile = request.user.userprofile
+
+    # Convert GEOS geometry to WKT string
+    if profile.location:
+        profile.location = profile.location.wkt
+
+    profile_form = UserProfileForm(instance=profile)
 
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
         profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
+
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
