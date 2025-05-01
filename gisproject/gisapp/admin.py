76d13c86974c583f631bde955ea807e9e95b2e1b
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-05-01 01:10:08 -0400

    AAdded User Profile to Django Admin

--- gisproject/gisapp/admin.py
+++ gisproject/gisapp/admin.py
@@ -1,3 +1,11 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.gis import admin as gis_admin
+from django.contrib.gis.db import models as gis_models
+from leaflet.admin import LeafletGeoAdmin  # Import from leaflet
 
-# Register your models here.
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(LeafletGeoAdmin):  # Use LeafletGeoAdmin instead of OSMGeoAdmin
+    list_display = ['user', 'home_address', 'phone_number']
--- gisproject/gisapp/forms.py
+++ gisproject/gisapp/forms.py
@@ -1,15 +1,28 @@
-# forms.py
 from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile
 from django.contrib.gis.geos import Point
+
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
 
+
 class UserProfileForm(forms.ModelForm):
+    location = forms.CharField(widget=forms.HiddenInput(), required=False)  # raw POINT string
+
     class Meta:
         model = UserProfile
         fields = ['home_address', 'phone_number', 'location']
+
+    def clean_location(self):
+        location = self.cleaned_data.get('location')
+        if location:
+            try:
+                lng, lat = location.replace("POINT(", "").replace(")", "").split()
+                return Point(float(lng), float(lat))
+            except Exception as e:
+                raise forms.ValidationError("Invalid location format.")
+        return None
