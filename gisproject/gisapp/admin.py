da193be7a62c9fe7bfdefbb1f802db1abe6968b7
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-05-04 00:04:45 -0400

    Added Django Admin capability for user registered. User registered from /register is a staff

--- gisproject/gisapp/admin.py
+++ gisproject/gisapp/admin.py
@@ -1,11 +1,27 @@
-# admin.py
 from django.contrib import admin
-from django.contrib.gis import admin as gis_admin
-from django.contrib.gis.db import models as gis_models
-from leaflet.admin import LeafletGeoAdmin  # Import from leaflet
-
 from .models import UserProfile
 
-@admin.register(UserProfile)
-class UserProfileAdmin(LeafletGeoAdmin):  # Use LeafletGeoAdmin instead of OSMGeoAdmin
-    list_display = ['user', 'home_address', 'phone_number']
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'home_address', 'phone_number')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        # Superusers can see all
+        if request.user.is_superuser:
+            return qs
+        # Staff users only see their own profile
+        return qs.filter(user=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is None:
+            return True
+        # Only allow users to change their own profile
+        return obj.user == request.user or request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return True
+        # Only allow users to delete their own profile
+        return obj.user == request.user or request.user.is_superuser
+
+admin.site.register(UserProfile, UserProfileAdmin)
