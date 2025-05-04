db123745a8f8b12f03c3a15cb3642743cf0b7b4b
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-05-04 00:12:17 -0400

    Fixed not showing user profile for registered user through /register

--- gisproject/gisapp/views.py
+++ gisproject/gisapp/views.py
@@ -9,20 +9,59 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
-# Register View
+# # Register View
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             messages.success(request, f'Account created for {user.username}!')
+#             return redirect('profile')
+#         else:
+#             messages.error(request, 'There was an error in registration.')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'register.html', {'form': form})
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import UserProfile  # adjust if your model is elsewhere
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+
+            # Make the user a staff member
+            user.is_staff = True
+            user.save()
+
+            # Give "Can view user profile" permission
+            content_type = ContentType.objects.get_for_model(UserProfile)
+            permission = Permission.objects.get(
+                codename='view_userprofile',
+                content_type=content_type
+            )
+            user.user_permissions.add(permission)
+
+            # Log the user in after successful registration
             login(request, user)
-            messages.success(request, f'Account created for {user.username}!')
+
+            messages.success(request, f'Account created for {user.username}, marked as staff, and permission granted!')
             return redirect('profile')
         else:
             messages.error(request, 'There was an error in registration.')
     else:
         form = UserCreationForm()
+        
     return render(request, 'register.html', {'form': form})
+
 
 # Login View
 def user_login(request):
